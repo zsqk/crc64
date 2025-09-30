0c292a647ff044001ad00f495745bc3e60ae1a2b
iugo <iugogogo@gmail.com>
2025-09-30 10:41:57 +0800

    chore: bump version to 0.1.6 in deno.json

---
+++ build/release.d.ts
@@ -0,0 +1,8 @@
+/** Exported memory */
+export declare const memory: WebAssembly.Memory;
+/**
+ * assembly/crc64/crc64
+ * @param buf `~lib/arraybuffer/ArrayBuffer | null`
+ * @returns `~lib/string/String`
+ */
+export declare function crc64(buf: ArrayBuffer | null): string;
