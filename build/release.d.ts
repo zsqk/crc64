5a12f04c02ac64136e97edee666d040f6f47aa65
iugo <iugogogo@gmail.com>
2025-09-30 11:29:38 +0800

    chore: new version for jsr.io (#8)
    * chore: update version to 0.1.5 and remove wasm export from deno.json
    
    * chore: bump version to 0.1.6 in deno.json
    
    * feat: add TypeScript definitions and update exports in deno.json
    
    * chore: bump version to 0.1.7 in deno.json
    
    * fix: add return type to async function for clarity in release.ts
    
    * fix: refactor export handling for memory and crc64 in release.ts
    
    * fix: update WASM loading logic for Node.js and Bun, and correct import path in tests
    
    * docs: update README.md with usage example for crc64 function
    chore: bump version to 0.1.9 in deno.json

---
+++ build/release.d.ts
@@ -0,0 +1,8 @@
+/** Exported memory */
+export declare const memory: WebAssembly.Memory;
+/**
+ * assembly/crc64/crc64
+ * @param buf `ArrayBuffer | null`
+ * @returns `string`
+ */
+export declare function crc64(buf: ArrayBuffer | null): string;
